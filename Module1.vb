cb5773fef4b8caef86f872aeb21498d606126d87
chedy <chedy@CHADOUMIX>
2025-11-23 14:14:02 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Windows.Forms
+Module Module1
+    Sub Main()
+        Dim nom As String
+        Dim prenom As String
+        Dim age As Integer
+
+        nom = InputBox("etrez votre nom")
+        prenom = InputBox("entrez votre prenom")
+        age = InputBox("entrez votre age")
+        MessageBox.Show("le nom est " & nom & "le prenom est " & prenom & "l age est " & age)
+
+
+
+
+    End Sub
+
+End Module
